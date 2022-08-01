633b909c2967a419c9bef245a0b7861a62dd25fc
KOKIAOKI <180447001@ccmailg.meijo-u.ac.jp>
2022-08-01 09:25:56 +0900

    on the way

--- icp_kdtree.py
+++ icp_kdtree.py
@@ -14,10 +14,6 @@ import scipy.linalg as linalg
 from matplotlib import cm
 
 
-trj_fig = plt.figure("Trjectory", figsize=(16, 9), dpi=120)
-ax_trj = trj_fig.add_subplot(111)
-
-
 class Pose2D:
     def __init__(self):
         self.x = ""
@@ -91,6 +87,9 @@ class ICPProcess:
     def getItr(self):
         return self.itr
 
+    def getEstPose(self):
+        return self.pose_min
+
 
     def ICP_scan_matching(self, current_pose):
         self.itr = 1
@@ -105,22 +104,21 @@ class ICPProcess:
                 new_pose, ev= self.gradient(current_pose) # 勾配法
             elif self.mode == 1:
                 new_pose, ev= self.Newton(current_pose) # ニュートン法
-            # elif self.mode == 2:
-            #     new_pose ,ev= cg(target_cloud, scan_cloud, current_pose) # 共役勾配法
+            elif self.mode == 2:
+                new_pose ,ev= self.cg(current_pose) # 共役勾配法
 
             current_pose = new_pose
 
             if ev < evmin: #前のスコアより低ければ最適解候補を更新
-                pose_min = new_pose
+                self.pose_min = new_pose
                 evmin = ev
-                trj_array.x = np.append(trj_array.x, np.array([[pose_min.x]]), axis=0)
-                trj_array.y = np.append(trj_array.y, np.array([[pose_min.y]]), axis=0)
-                trj_array.th = np.append(trj_array.th, np.array([[pose_min.th]]), axis=0)
+                trj_array.x = np.append(trj_array.x, np.array([[self.pose_min.x]]), axis=0)
+                trj_array.y = np.append(trj_array.y, np.array([[self.pose_min.y]]), axis=0)
+                trj_array.th = np.append(trj_array.th, np.array([[self.pose_min.th]]), axis=0)
                 
             if self.itr > 30:
                 break
             self.itr += 1
-        return pose_min
             
 
     # 勾配法
@@ -138,7 +136,6 @@ class ICPProcess:
         ev = np.sum(dists**2) / self.scan_points_num
         evmin = ev
         evold = 100000
-        print("ev",ev)
         while abs(evold - ev) > self.evthere:
             evold = ev
 
@@ -158,6 +155,7 @@ class ICPProcess:
                 evmin = ev
                 txmin = copy.deepcopy(t_)
         return(txmin, evmin)
+
 
     # Newton法
     def Newton(self, init_pose):
@@ -188,6 +186,11 @@ class ICPProcess:
         evmin = self.calcValue(t_.x, t_.y, t_.th)
         txmin = copy.deepcopy(t_)
         return(txmin, evmin)
+
+    # def cg(self, init_pose):
+
+
+
 
 
     # 勾配計算用の微小変位
@@ -261,7 +264,6 @@ class ICPProcess:
             self.ax_init_fig.cla()
             init_temp_cloud = self.transpointcloud(self.scan_cloud, current_pose)
             self.output_init_graph(init_temp_cloud)
-            print(init_temp_cloud)
             self.init_fig.show()
             continue_init = int(input("Are you sure you want to conduct ICP from this pose? No:0 Yes:1 >>"))
         return current_pose
@@ -291,6 +293,45 @@ class ICPProcess:
             vis2_temp = self.ax_kd_tree.plot([target_cloud[index, 0], scan_cloud[i, 0]], [target_cloud[index, 1], scan_cloud[i, 1]], "-g")
             vis2.extend(vis2_temp)
         self.frames_kdtree.append(vis0 + vis1 + vis2)
+
+
+    # 総当りの分布と軌跡のグラフ
+    def trj_graph(self, itr):
+        trj_fig = plt.figure("Trjectory", figsize=(16, 9), dpi=120)
+        ax_trj = trj_fig.add_subplot(111)
+        width_offset = 0.05
+        max_offset = 1.0
+        points = int((max_offset/width_offset)*2 + 1)
+        offset_array = Array2D() 
+        for i in range(points):
+            for j in range(points):
+                offset_pose = Pose2D()
+                offset_pose.x = self.pose_min.x + width_offset * i - max_offset
+                offset_pose.y = self.pose_min.y + width_offset * j - max_offset
+                offset_pose.th = 0
+                offset_cloud = self.transpointcloud(self.scan_cloud, offset_pose)
+                err_sum, indexes_dist = self.kd_tree.query(offset_cloud)
+                err_sum_av = np.sum(err_sum) / self.scan_points_num
+                offset_array.x = np.append(offset_array.x, np.array([[offset_pose.x]]), axis=0)
+                offset_array.y = np.append(offset_array.y, np.array([[offset_pose.y]]), axis=0)
+                offset_array.ev = np.append(offset_array.ev, np.array([[err_sum_av]]), axis=0)
+        ex_len = len(offset_array.ev)
+        length_tmp = int(np.sqrt(ex_len))
+        X_dist = offset_array.x.reshape(length_tmp,length_tmp)
+        Y_dist = offset_array.y.reshape(length_tmp,length_tmp)
+        EX_dist = offset_array.ev.reshape(length_tmp,length_tmp)
+        er_min = min(offset_array.ev)
+        er_max = max(offset_array.ev)
+        ax_hmap = ax_trj.pcolor(X_dist, Y_dist, EX_dist, cmap=cm.jet, vmin=er_min, vmax=er_max)
+        ax_trj.plot(trj_array.x,trj_array.y,'or',linestyle='solid')
+        plt.colorbar(ax_hmap, label='error average[m]')
+        ax_trj.text(0.1,1.05, 'iteration: {} '.format(itr), fontsize=15, transform=ax_trj.transAxes)
+        ax_trj.text(0.1,1.01, 'execution time[ms]: {} '.format(round(exe_time,2)), fontsize=15, transform=ax_trj.transAxes)
+        ax_trj.set_xlabel('x [m]')
+        ax_trj.set_ylabel('y [m]')
+        ax_trj.grid()
+        ax_trj.set_aspect('equal')
+        
 
 
 if __name__ == "__main__":
@@ -330,58 +371,30 @@ if __name__ == "__main__":
     trj_array.th = np.append(trj_array.th, np.array([[current_pose.th]]), axis=0)
 
     # ICP
-    est_Pose = Pose2D()
     start_time = time.perf_counter()
-    print(current_pose.x)
-    est_Pose = icp.ICP_scan_matching(current_pose)
+    icp.ICP_scan_matching(current_pose)
     end_time = time.perf_counter()
     exe_time = (end_time - start_time)*1000
-    matched_cloud = icp.transpointcloud(scan_cloud, est_Pose) #マッチングした点群
+
+    est_pose = icp.getEstPose()
     indexes = icp.getIndexes()
     itr = icp.getItr()
+
+    matched_cloud = icp.transpointcloud(scan_cloud, est_pose) #マッチングした点群
     icp.output_anim_graph(target_cloud, matched_cloud, indexes) #マッチングしたときの点群をアニメーションに追加
     
     # 出力
-    print("estimated pose:","x",est_Pose.x,"y",est_Pose.y,"theta",est_Pose.th)
+    print("estimated pose:","x",est_pose.x,"y",est_pose.y,"theta",est_pose.th)
     print("iteration:",itr)
     print("exe_time:",exe_time,"[ms]")
 
     ani = animation.ArtistAnimation(icp.kdtree_fig, icp.frames_kdtree, interval=500, blit=True, repeat_delay=1000)  #アニメーション
-    # ani.save(output_name + 'convergence_animation.mp4')
+    icp.trj_graph(indexes, itr) # 軌跡のグラフ
 
-    #軌跡
-    # width_offset = 0.01
-    # max_offset = 1.0
-    # points = int((max_offset/width_offset)*2 + 1)
-    # offset_array = Array2D() 
-    # for i in range(points):
-    #     for j in range(points):
-    #         offset_pose = Pose2D()
-    #         offset_pose.x = est_Pose.x + width_offset * i - max_offset
-    #         offset_pose.y = est_Pose.y + width_offset * j - max_offset
-    #         offset_pose.th = 0
-    #         offset_cloud = transpointcloud(scan_cloud, offset_pose)
-    #         err_sum, indexes_dist = kd_tree.query(offset_cloud)
-    #         err_sum_av = np.sum(err_sum) / scan_points_num
-    #         offset_array.x = np.append(offset_array.x, np.array([[offset_pose.x]]), axis=0)
-    #         offset_array.y = np.append(offset_array.y, np.array([[offset_pose.y]]), axis=0)
-    #         offset_array.ev = np.append(offset_array.ev, np.array([[err_sum_av]]), axis=0)
-    # ex_len = len(offset_array.ev)
-    # length_tmp = int(np.sqrt(ex_len))
-    # X_dist = offset_array.x.reshape(length_tmp,length_tmp)
-    # Y_dist = offset_array.y.reshape(length_tmp,length_tmp)
-    # EX_dist = offset_array.ev.reshape(length_tmp,length_tmp)
-    # er_min = min(offset_array.ev)
-    # er_max = max(offset_array.ev)
-    # ax_hmap = ax_trj.pcolor(X_dist, Y_dist, EX_dist, cmap=cm.jet, vmin=er_min, vmax=er_max)
-    # ax_trj.plot(trj_array.x,trj_array.y,'or',linestyle='solid')
-    # plt.colorbar(ax_hmap, label='error average[m]')
-    # ax_trj.text(0.1,1.05, 'iteration: {} '.format(itr), fontsize=15, transform=ax_trj.transAxes)
-    # ax_trj.text(0.1,1.01, 'execution time[ms]: {} '.format(round(exe_time,2)), fontsize=15, transform=ax_trj.transAxes)
-    # ax_trj.set_xlabel('x [m]')
-    # ax_trj.set_ylabel('y [m]')
-    # ax_trj.grid()
-    # ax_trj.set_aspect('equal')
-    # trj_fig.savefig(output_name + "trj.png")
-
+    # グラフ保存
+    # if sys.version_info < (3,7):
+    #     ani.save(output_name + 'convergence_animation.gif')
+    # else:
+    #     ani.save(output_name + 'convergence_animation.mp4')
+    icp.trj_fig.savefig(output_name + "trj.png")
     plt.show()
